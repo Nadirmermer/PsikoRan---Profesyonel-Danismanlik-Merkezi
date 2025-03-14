2ffad9fe714cc0eea6635fdd2c2063273ab711c7
Nadir Mermer <210209073@ogrenci.ibu.edu.tr>
2025-03-14 05:06:58 +0300

    logo2

---
+++ src/utils/logoUtils.ts
@@ -0,0 +1,9 @@
+import logoImage from '../assets/logo/main-logo.png';
+
+/**
+ * Logo görselini PDF için hazırlar
+ * @returns Logo görselinin URL'si
+ */
+export function getLogoUrl(): string {
+  return logoImage;
+} 
